548a6032ce11e822de66bcd0d49ec24fd121a9b2
mv-mnl <mmelendez1505@gmail.com>
2026-02-21 22:23:45 -0600

    fix: correcion en el rm.sh

--- rm.sh
+++ rm.sh
@@ -1,3 +1,13 @@
-rm -r sigo_no_me_siguen.txt me_siguen_no_sigo.txt followers.txt following.txt followers_and_following.html
+#!/bin/bash
 
-rm -rf connections/
+# 1. Activamos las opciones extendidas de Bash
+shopt -s extglob
+
+# 2. Ejecutamos el borrado ignorando tus archivos importantes
+# La sintaxis !(file1|file2) significa "todo lo que NO sea esto"
+rm -rf !(.git|.gitignore|analyze_instagram.py|open_profiles.py|README.md|rm.sh)
+
+# 3. Desactivamos la opción (opcional, por limpieza)
+shopt -u extglob
+
+echo "Limpieza completada: Solo quedan tus archivos de código y configuración."
